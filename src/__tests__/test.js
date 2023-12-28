de7933664a776d5774ea1a533e8c9131e25e6f48
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 17:08:48 +0500

    Update and rename test.app.js to test.js

---
+++ src/__tests__/test.js
@@ -0,0 +1,16 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+test('тоже должно работать аналогично предыдущей задачи', async () => {
+  const expected = JSON.stringify({
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  });
+  const data = await GameSavingLoader.load();
+  expect(data).toEqual(expected);
+});
