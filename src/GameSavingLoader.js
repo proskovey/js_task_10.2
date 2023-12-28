4946fa97064718aa1514d0852cdb4a4cb4364cf9
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 17:08:05 +0500

    Create GameSavingLoader.js

---
+++ src/GameSavingLoader.js
@@ -0,0 +1,10 @@
+import json from './parser';
+import read from './reader';
+
+export default class GameSavingLoader {
+  static async load() {
+    const response = await read();
+    const jsonResponse = await json(response);
+    return jsonResponse;
+  }
+}
